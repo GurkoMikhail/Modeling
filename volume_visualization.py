378b5b114d467e557f760c169926afba2609c1a9
GurkoMikhail <gurkomihail@gmail.com>
2021-01-28 16:35:42 +0800

    volume vis

--- volume_visualization.py
+++ volume_visualization.py
@@ -1,20 +1,48 @@
 from PyQt5 import QtGui
+from PyQt5.QtWidgets import QWidget
 from PyQt5.uic import loadUi
+from numpy import array, asarray, load, min, max, arange, nonzero, uint32, float32, NaN
 from pyqtgraph.Qt import mkQApp
-from pyqtgraph.graphicsItems.HistogramLUTItem import HistogramLUTItem
-from pyqtgraph.opengl.items.GLAxisItem import GLAxisItem
-from pyqtgraph.opengl.items.GLBoxItem import GLBoxItem
+from pyqtgraph import GradientEditorItem, makeRGBA
+from pyqtgraph.opengl import GLBoxItem, GLVolumeItem
+
+voxel_size = 0.4
+
+volume = load('Phantoms/efg3.npy').astype(float32)
+volume_size = array(volume.shape, dtype=float32)*voxel_size
+
+levels = [
+    min(volume),
+    max(volume)
+]
+
+def gradientChanged():
+    global volume, volumeItem, gradientEditor, levels
+    listTicks = gradientEditor.listTicks()
+    listTicks[0][0].color.setAlpha(0)
+    for tick in listTicks[1:]:
+        tick[0].color.setAlpha(30)
+    lut = gradientEditor.getLookupTable(255)
+    volume_colors = asarray([makeRGBA(data=slice, lut=lut, levels=levels)[0] for slice in volume])
+    volumeItem.setData(volume_colors)
 
 mkQApp()
 mainWindow = loadUi("UI/volume_visualization.ui")
 
-hist = HistogramLUTItem()
-# hist.setImageItem(image)
-mainWindow.graphicsView_histogram.addItem(hist)
+volumeItem = GLVolumeItem(None, sliceDensity=2)
+volumeItem.scale(*[voxel_size]*3)
+gradientEditor = GradientEditorItem(orientation='right')
+gradientEditor.sigGradientChangeFinished.connect(gradientChanged)
+mainWindow.graphicsView.addItem(gradientEditor)
+volumeViewWidget = mainWindow.openGLWidget
+volumeViewWidget.addItem(volumeItem)
+
+volumeViewWidget.setCameraPosition(distance=volume_size[0]*3)
+volumeViewWidget.pan(*volume_size/2)
 
 space_box = GLBoxItem()
-space_box.setSize(10, 10, 10)
-mainWindow.graphicsView_volume.addItem(space_box)
+space_box.setSize(*volume_size)
+mainWindow.openGLWidget.addItem(space_box)
 
 
 mainWindow.show()
