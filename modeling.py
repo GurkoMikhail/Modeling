05153e718f7961d6a26bac3b00c41dc688d4965e
GurkoMikhail <gurkomihail@gmail.com>
2021-03-09 23:41:02 +0700

    fix .random.

--- modeling.py
+++ modeling.py
@@ -259,10 +259,10 @@ class Source:
         self.rotated = False
         if euler_angles is not None:
             self.rotate(euler_angles, rotation_center)
-        self.rng_dist = np.rand.RandState()
-        self.rng_ddist = np.rand.RandState()
-        self.rng_time = np.rand.RandState()
-        self.rng_dir = np.rand.RandState()
+        self.rng_dist = np.random.RandState()
+        self.rng_ddist = np.random.RandState()
+        self.rng_time = np.random.RandState()
+        self.rng_dir = np.random.RandState()
 
     def rotate(self, euler_angles, rotation_center=None):
         self.rotated = True
