b4882550c7609b5b256fe22832764159eb9c0676
GurkoMikhail <gurkomihail@gmail.com>
2021-03-09 23:26:34 +0700

    change random_state

--- modeling.py
+++ modeling.py
@@ -259,10 +259,10 @@ class Source:
         self.rotated = False
         if euler_angles is not None:
             self.rotate(euler_angles, rotation_center)
-        self.rng_dist = np.random.default_rng()
-        self.rng_ddist = np.random.default_rng()
-        self.rng_time = np.random.default_rng()
-        self.rng_dir = np.random.default_rng()
+        self.rng_dist = np.random.RandomState()
+        self.rng_ddist = np.random.RandomState()
+        self.rng_time = np.random.RandomState()
+        self.rng_dir = np.random.RandomState()
 
     def rotate(self, euler_angles, rotation_center=None):
         self.rotated = True
