69a76774ff5bab96fb9d8ca8183964d62e1c8902
GurkoMikhail <gurkomihail@gmail.com>
2021-03-12 03:06:30 +0700

    72 proc

--- main_knl.py
+++ main_knl.py
@@ -59,10 +59,10 @@ def createProcess(time, lock):
     modeling.startMP(time, lock)
 
 if __name__ == '__main__':
-    processesNumber = 287
+    processesNumber = 72
     time = mp.Queue()
     lock = mp.Lock()
-    startTime = np.round(np.arange(0., 287.*2/100, 0.01), 2)
+    startTime = np.round(np.arange(0., 72.*4/100, 0.01), 2)
     for st in startTime:
         time.put((st, round(st + 0.01, 2)))
     processes = []
