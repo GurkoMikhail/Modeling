0023b0d29a7d69b7748e82fe4491239ef73aff2c
GurkoMikhail <gurkomihail@gmail.com>
2021-03-20 15:19:16 +0700

    different colimators for knl

--- main_knl.py
+++ main_knl.py
@@ -2,75 +2,100 @@ import numpy as np
 from subjects import Space, Phantom, Collimator, Detector
 from modeling import Source, Modeling
 import multiprocessing as mp
+from materials import Materials
 from time import sleep
 
-def createProcess(time, lock):
-    size = np.asarray((53.3, 60., 38.7))
-    space = Space(size, 0)
-
-    phantom = np.load('Phantoms/ae3_fix.npy')
-    phantom = Phantom(
-        coordinates=(1.05, (12.4 - 10.) + 5, -3.),
-        material=phantom,
-        voxel_size=0.4
-        )
-    space.add_subject(phantom)
-
+def createProcess(parameters):
     detector = Detector(
         coordinates=(0., 9.5, 0.),
-        size=(53.3, 38.7, 9.5),
+        size=(51.2, 40., 9.5),
         material=4,
         euler_angles=(0, np.pi/2, 0),
         rotation_center=(0., 0., 0.)
         )
-    space.add_subject(detector)
 
     collimator = Collimator(
-        coordinates=(detector.coordinates[0], detector.coordinates[1] + 0.5 + 2.4, detector.coordinates[2]),
-        size=(*detector.size[:2], 2.4),
+        coordinates=(detector.coordinates[0], detector.coordinates[1] + 0.5 + parameters['Collimator thickness'], detector.coordinates[2]),
+        size=(*detector.size[:2], parameters['Collimator thickness']),
         material=5,
-        hole_diameter=0.111,
-        septa=0.016,
+        hole_diameter=parameters['Hole diameter'],
+        septa=parameters['Septa'],
         euler_angles=detector.euler_angles,
         rotation_center=detector.rotation_center
         )
+
+    phantom = np.load('Phantoms/ae3cut.npy')
+    phantom = Phantom(
+        coordinates=(0., collimator.coordinates[1] - 5., 0.),
+        material=phantom,
+        voxel_size=0.4
+        )
+
+    size = np.asarray((detector.size[0], phantom.coordinates[1] + phantom.size[1], detector.size[2]))
+    space = Space(size, 0)
+    space.add_subject(detector)
     space.add_subject(collimator)
+    space.add_subject(phantom)
 
     source = Source(
         coordinates=phantom.coordinates,
         activity=300*10**6,
-        distribution=np.load('Phantoms/efg3_fix.npy'),
+        distribution=np.load('Phantoms/efg3cut.npy'),
         voxel_size=0.4,
         radiation_type='Gamma',
         energy=140.5*10**3,
         half_life=6*60*60
         )
 
+    materials = {
+        'Compounds and mixtures/Air, Dry (near sea level)':         0,
+        'Compounds and mixtures/Lung':                              1,
+        'Compounds and mixtures/Tissue, Soft (ICRU-44)':            2,
+        'Compounds and mixtures/B-100 Bone-Equivalent Plastic':     3,
+        'Compounds and mixtures/Sodium Iodide':                     4,
+        'Elemental media/Pb':                                       5,
+    }
+
+    materials = Materials(materials, max_energy=140500)
+
     modeling = Modeling(
         space,
         source,
-        solid_angle=None,
-        file_name='efg3_fix 0.0 deg full.hdf',
-        start_time=0.,
+        materials,
+        solid_angle=((0, -1, 0), 15*np.pi/180),
         time_step=0.01,
-        subject=detector
+        subject=detector,
+        file_name=parameters['Name']
         )
     
-    modeling.startMP(time, lock)
+    modeling.startMP(parameters['Time'], parameters['Lock'])
 
 if __name__ == '__main__':
-    processesNumber = 72
-    time = mp.Queue()
-    lock = mp.Lock()
-    startTime = np.round(np.arange(0., 72.*4/100, 0.01), 2)
-    for st in startTime:
-        time.put((st, round(st + 0.01, 2)))
+    cores_number = 8
+    totalTime = 15.
+    parameters = mp.Queue()
+    locks = [mp.Lock() for i in range(4)]
+    startTime = np.round(np.linspace(0, totalTime, cores_number), 2)
+    finishTime = np.round((startTime + (startTime[1] - startTime[0])), 2)
+    times = np.stack((startTime, finishTime), axis=1)
     processes = []
-    for i in range(processesNumber):
-        process = mp.Process(target=createProcess, args=(time, lock))
-        processes.append(process)
-        process.start()
-        sleep(10)
+    collimatorThickness = [2.405, 2.405, 3.58, 4.064]
+    holeDiameter = [0.111, 0.145, 0.116, 0.294]
+    septa = [0.016, 0.02, 0.013, 0.114]
+    names = ['LEHR', 'LEAP', 'LEUHR', 'ME']
+    for i, lock in enumerate(locks):
+        for time in times:
+            parameters = {
+                'Lock': lock,
+                'Time': time,
+                'Collimator thickness': collimatorThickness[i],
+                'Hole diameter': holeDiameter[i],
+                'Septa': septa[i],
+                'Name': names[i]
+                }
+            process = mp.Process(target=createProcess, args=(parameters, ))
+            processes.append(process)
+            process.start()
     for process in processes:
         process.join()
     print('End!')
